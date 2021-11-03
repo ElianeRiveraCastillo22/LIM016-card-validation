b8f965504483a5cbbaa12c896aab54f51f48aaba
ElianeRiveraCastillo22 <elianercast@gmail.com>
2021-11-03 10:49:45 -0500

    termine el validator js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+// para que aparezca los numeros en la tarjeta 
+/*function aparecer(){
+    document.getElementById("numero").innerHTML="1234567";
+}*/
+//imput numero de trajeta 
+let inputNumero=document.getElementById("input-Numero");
+let inputNombre=document.getElementById("input-Nombre")
+let botonValidar= document.getElementById("button-continuar");
+
+botonValidar.addEventListener('click', ()=>{
+    let valorDelNumero = inputNumero.value;// elimine .replace
+//para que aparezca el numero en la tarjeta 
+    let apareceNumero=document.getElementById("aparece-numero");
+    apareceNumero.innerText=validator.maskify(valorDelNumero)
+ let isValidC=validator.isValid(valorDelNumero)
+ let maskifyC= validator.maskify(valorDelNumero)
+ 
+ if (isValidC){
+     alert("tu tarjeta es valida")
+    valorDelNumero = maskifyC
+     
+} else{
+        alert("tu tarjeta no es valida")
+          }
+          
+ });
+ //restricciones para el input del numero de tarjeta 
+inputNumero.addEventListener('keyup',(e)=>{
+    let valorInput= e.target.value; 
+    inputNumero.value = valorInput.replace(/\s/g, '').replace(/\D/g, '');//.replace(/([0-9]{4})/g,'$1 ').trim();
+    //elimianmos espacios en blanco .replace(/\s/g, '')eliminar las letras .replace(/\D/g, '')//ponemos espacios cada cuatro numeros.replace(/(\d {4})/g,'$number ').trim();
+    
+}  
+);
+//restricciones para el input del nombre de tarjeta 
+inputNombre.addEventListener('keyup',(e)=>{
+      let valorDelNombre= e.target.value;
+   inputNombre.value=valorDelNombre.replace(/[0-9]/g,'')
+   let apareceNombre=document.getElementById("aparece-nombre")
+   apareceNombre.innerText=valorDelNombre
+
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
 const validator = {
-  // ...
-};
+  //*validar algoritmo de lunh
+
+  isValid:function (valorDelNumero){
+
+    //* convertir en array y volteo los datos
+
+     let valorInvertido= Array.from(valorDelNumero).map((i)=>Number(i)).reverse();
+     let sumaDeValores = 0;
+     
+     //*ubicar las posiones pares para multiplicarlo 
+    for(let i = 0; i < valorInvertido.length; i++){ 
+      
+          if(i % 2 != 0){//busca las posiones inpares por que esas con las pares 
+                if ( (valorInvertido[i]*2) >=10){
+                  valorInvertido[i]=valorInvertido[i]*2-9;
+                } 
+                else{
+                  valorInvertido[i]= valorInvertido[i]*2;
+                }
+          } 
+          else {
+            valorInvertido[i];
+                  } 
+          sumaDeValores=sumaDeValores+valorInvertido[i];
+}
+return sumaDeValores%10===0 
+},
+
+  maskify: function (valorDelNumero){ 
+   // pasamos a un arrray y juntamos los espacios 
+   let informacionDelArray= Array.from(valorDelNumero);
+
+   let cuatroUltimosNumeros= informacionDelArray.slice(-4);
+   let todoMenosCuatro=informacionDelArray.slice(0,-4);
+   //console.log(todoMenosCuatro)
+   for(let i = 0 ; i<valorDelNumero.length-4 ; i++){ 
+   //igualamos los numeros con el # y guardamos mapeando segun la condicion
+   todoMenosCuatro[i]="#";
+   //todoMenosCuatro= todoMenosCuatro[i];
+   }
+   // convinado los numeros 
+   let maskifyCard = (todoMenosCuatro.concat(cuatroUltimosNumeros)).join("");
+   //console.log(maskifyCard)
+   return maskifyCard
+}  
+} 
 
 export default validator;
+
